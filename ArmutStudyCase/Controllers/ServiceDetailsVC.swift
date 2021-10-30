e44bb72ce180ffa8fd9cc357a6ede8ccada5ba94
Alican Kurt <kurta15@itu.edu.tr>
2021-10-30 16:48:26 +0300

    Created ServiceDetailsVC to show service details
    The ViewController has been designed on Main Storyboard.

---
+++ ArmutStudyCase/Controllers/ServiceDetailsVC.swift
@@ -0,0 +1,21 @@
+//
+//  ServiceDetailsVC.swift
+//  ArmutStudyCase
+//
+//  Created by Alican Kurt on 30.10.2021.
+//
+
+import UIKit
+
+class ServiceDetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+
+}
